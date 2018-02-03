e1bb9eab4f0e39f4bff17227f8dca37a60b7f851
kazalt <frederickdufresne@deepteal.com>
2018-02-03 14:55:16 -0500

    wip on websocket

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+const _ = require('lodash');
 const express = require('express');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = express();
 
+global.sessions = [];
+
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'pew paw'
+}));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 const sequelize = require('./init_database');
 
@@ -25,4 +32,29 @@ sequelize
 
 app.listen(3000);
 
+const usersSockets = {};
+const webSocketServer = new (require('ws')).Server({ port: 5000 });
+webSocketServer.on('connection', (socket, req) => {
+  createSession(req);
+  usersSockets[req.session.id] = socket;
+
+  socket.on('message', function(message) {
+
+  });
+});
+
 module.exports = app;
+
+////////////////
+
+function createSession(req) {
+  req.session.user = {
+    id: _.max(global.sessions, 'id'),
+    position: {
+      x: req.body.position.x,
+      y: req.body.position.y
+    }
+  };
+
+  global.sessions.push(req.session.user);
+}
