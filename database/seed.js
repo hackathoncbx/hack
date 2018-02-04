abc86ec50bb456ab9a8c3761c3811b654aa7cb8b
brierean <briere.anthony@outlook.com>
2018-02-04 00:22:48 -0500

    helpfix + data

--- database/seed.js
+++ database/seed.js
@@ -1,6 +1,5 @@
 module.exports = function(sequelize, options) {
-  const seeds = ["first_responder", "defibrillators", "help"];
-
+  const seeds = ["first_responder", "defibrillators", "help", "helpsteps"];
   return seeds.reduce(function(cur, next) {
     return cur.then(function() {
       return require('./seeds/' + next)(sequelize, options);
