1e8275a23b3eae3d36c8d2734f640327033f0a33
Pascal Godin <godin_boy@msn.com>
2018-02-04 00:39:29 -0500

    fix seed

--- database/seed.js
+++ database/seed.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, options) {
-  const seeds = ["first_responder", "defibrillators", "help", "helpsteps"];
+  const seeds = ["first_responder", "defibrillators", "help", "helpsteps", "drug_stores_and_hospitals"];
   return seeds.reduce(function(cur, next) {
     return cur.then(function() {
       return require('./seeds/' + next)(sequelize, options);
